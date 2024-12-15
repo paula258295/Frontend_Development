3f55f2bc36949b0f7faf23b9b79d168c5226821f
Paulina <paulina.krajnik04@gmail.com>
2024-12-15 21:29:55 +0100

    lab3

---
+++ lab3/app/page.js
@@ -0,0 +1,9 @@
+import React from 'react'; // Jeśli używasz React starszego niż wersja 17
+
+
+function Homepage() {
+    return (
+     <div> To jest strona główna </div>
+    )
+  };
+  export default Homepage;
